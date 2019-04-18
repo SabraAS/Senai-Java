52d93dec28edcbbfd8f17cd3a1014c308db85c04
Sabrina <sabrinaangelimsampaio@gmail.com>
2019-04-17 21:35:15 -0300

    Polimorfismo

---
+++ 13_Polimorfismo/src/controller/Teste.java
@@ -0,0 +1,23 @@
+package controller;
+
+import model.ContaCorrente;
+import model.ContaEspecial;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		ContaCorrente cc = new ContaCorrente("Michael Oliveira", "0001", "123456",
+				250);
+		System.out.println("Titular: " + cc.getTitular());
+		System.out.println("Saldo: " + cc.calcularSaldo());
+		
+		ContaEspecial cce = new ContaEspecial("Fabrício Curvello", "0034", "987654",
+				0.50, 500);
+		
+		System.out.println("Titular: " + cce.getTitular());
+		System.out.println("Saldo: " + cce.calcularSaldo());
+
+	}
+
+}
