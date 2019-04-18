724f5749f49c8aa78792e43d662f5ed9bf57ece1
Sabrina <sabrinaangelimsampaio@gmail.com>
2019-04-17 21:23:25 -0300

    HelloWorld

---
+++ 01_HelloWorld/src/controller/HelloWorld.java
@@ -0,0 +1,40 @@
+package controller;
+
+// Este é o meu primeiro programa em JAVA
+
+/*
+ * Comentário de várias linhas,
+ * tambám conhecido como
+ * comentário de bloco.
+ */
+
+/**
+ * Comentário JAVADOC
+ * 
+ * @author aluno
+ *
+ */
+
+public class HelloWorld {
+	
+	//método main
+	public static void main(String[] args) {
+
+		//Escreval("Olá Mundo!")
+		System.out.println("Olá Mundo!");
+
+	}//fim do método main
+
+}// fim da classe
+
+
+
+
+
+
+
+
+
+
+
+
