c76832b0380276886e1fc0743da660254ea18ecd
Sabrina <sabrinaangelimsampaio@gmail.com>
2019-04-17 21:26:48 -0300

    Tipos Primitivos

---
+++ 05_TiposPrimitivos/src/controller/ExemploInteiro.java
@@ -0,0 +1,17 @@
+package controller;
+
+public class ExemploInteiro {
+
+	public static void main(String[] args) {
+		
+		int num1, num2, soma;
+		
+		num1 = 12;
+		num2 = 3;
+		soma = num1 + num2;
+		
+		System.out.println("Valor da Soma: " + soma);
+		
+	}
+
+}
